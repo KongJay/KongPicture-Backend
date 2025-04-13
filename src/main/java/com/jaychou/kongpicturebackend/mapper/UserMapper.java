004632215af313830fb52dae9acd15690ecab4c2
JayChou <996774254@qq.com>
2025-04-13 23:57:54 +0800

    用户模块开发完毕

---
+++ src/main/java/com/jaychou/kongpicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.jaychou.kongpicturebackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jaychou.kongpicturebackend.model.entity.User;
+
+/**
+* @author JayChou
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-04-13 22:53:15
+* @Entity com.jaychou.kongpicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/jaychou/kongpicturebackend/service/UserService.java
@@ -0,0 +1,65 @@
+package com.jaychou.kongpicturebackend.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.jaychou.kongpicturebackend.model.dto.user.UserQueryRequest;
+import com.jaychou.kongpicturebackend.model.entity.User;
+import com.jaychou.kongpicturebackend.model.vo.LoginUserVO;
+import com.jaychou.kongpicturebackend.model.vo.UserVO;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+* @author JayChou
+* @description 针对表【user(用户)】的数据库操作Service
+* @createDate 2025-04-13 22:53:15
+*/
+public interface UserService extends IService<User> {
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    UserVO getUserVO(User user);
+
+    List<UserVO> getUserVOList(List<User> userList);
+
+    QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest);
+
+    String getEncryptPassword(String userPassword);
+}
